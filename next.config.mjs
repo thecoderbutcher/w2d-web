10f609bdedb245c6d71bd44eedb6fce1ee055f27
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2025-01-24 00:25:50 -0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     distDir: 'docs',
     output: 'export',
-    assetPrefix: './',
+    assetPrefix: 'https://thecoderbutcher.github.io/w2d-web/',
     basePath: '/w2d-web'
 };
 
